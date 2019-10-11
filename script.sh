c033ec2b1b753ac603d52c6496d4edb893443064
Pierrick GRANDCLEMENT-CHAFFY <pierrick.grandclement@gmail.com>
2019-10-11 16:35:45 +0200

    Mozilla reset

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-echo "
+menu (){
+    clear
+    echo "
          . ,/(((/. .                                                           
         .(((((((((((                   **   ##              (,                 
        .(#%#(((((#%#(.               /*     ,,  *@&,    /     /,               
@@ -13,6 +15,48 @@ echo "
       (%@@@@@@@@@*#&@%#  .*   / ,* *,  /   *, /   .* ,.     *.  (   /  .*(     
          /@@@@@@@@@/                                                    /      
           .(#%%%((,                                                    *       "
-echo "Welcome in the IT-Akademy console"
-echo "What did you want to do ?"
-echo ""
+    echo ""
+    echo -e "Welcome in the \e[33mIT-Akademy\e[39m console"
+    echo "What did you want to do ?"
+    echo "[1] Remove your traces"
+    echo "[2] Install a virtual machine"
+    echo "[0] Quit"
+    echo ""
+    read -p "What is your choice ? " menuChoice
+}
+choiceRemoveMenu (){
+    clear
+    echo "What did you want to do ?"
+    echo "[1] Remove all your traces"
+    echo "[2] Select what you want to remove"
+    read -p "What is your choice ? " removeTracesChoice
+}
+menu
+    case $menuChoice in
+    0)
+        break
+    ;;
+    1)
+        choiceRemoveMenu
+        case $removeTracesChoice in
+        1)
+            echo "All your traces are going to be revomed ..."
+            rm -rf ~/.mozilla
+            read -p "Traces removed."
+        ;;
+        2)
+
+        ;;
+        *)
+            choiceRemoveMenu
+        ;;
+        esac
+    ;;
+    2)
+        echo "Going to install a VM"
+    ;;
+    *)
+        menu
+    ;;
+    esac
+echo "You decided to leave the console."
