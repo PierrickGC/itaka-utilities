c5060cdc85f9b80365a7a49e9f91d692b091798e
Kevin AMARAL <k.amaral@it-students.fr>
2019-10-11 17:24:55 +0200

    fix error

--- script.sh
+++ script.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-menu (){
+banner (){
     clear
+    echo " -------------- IT-Akademy --------------"
+    echo " "
+}
+
+changePwdMenu (){
+    read -p " Change PWD"
+    menu
+}
+
+vmInstall (){
+    read -p " VM installed"
+    menu
+}
+
+menu (){
+    banner
     echo "
          . ,/(((/. .                                                           
         .(((((((((((                   **   ##              (,                 
@@ -16,50 +32,58 @@ menu (){
          /@@@@@@@@@/                                                    /      
           .(#%%%((,                                                    *       "
     echo ""
-    echo -e "Welcome in the \e[33mIT-Akademy\e[39m console"
-    echo "What did you want to do ?"
-    echo "[1] Remove your traces"
-    echo "[2] Install a virtual machine"
-    echo "[0] Quit"
+    echo -e " Welcome in the \e[33mIT-Akademy\e[39m console"
+    echo " What did you want to do ?"
+    echo " [1] Remove your traces"
+    echo " [2] Change your pwd"
+    echo " [3] Install a virtual machine"
+    echo " [0] Exit"
     echo ""
-    read -p "What is your choice ? " menuChoice
+    read -p " What is your choice ? " menuChoice
+    case $menuChoice in
+    0)
+        exit
+        read -p " You decided to leave the console."
+    ;;
+    1)
+        choiceRemoveMenu
+    ;;
+    2)
+        changePwdMenu
+    ;;
+    3)
+        vmInstall
+     ;;
+    *)
+        menu
+    ;;
+    esac
 }
 choiceRemoveMenu (){
-    clear
-    echo "What did you want to do ?"
-    echo "[1] Remove all your traces"
-    echo "[2] Select what you want to remove"
-    read -p "What is your choice ? " removeTracesChoice
+    banner
+    echo " What did you want to do ?"
+    echo " [1] Remove all your traces"
+    echo " [2] Select what you want to remove"
+    echo " [0] Return to the menu"
+    echo ""
+    read -p " What is your choice ? " removeTracesChoice
     case $removeTracesChoice in
+        0)
+            menu
+        ;;
         1)
-            echo "All your traces are going to be revomed ..."
-            rm -rf ~/.mozilla
-            read -p "Traces removed."
+            echo " All your traces are going to be revomed ..."
+            ##rm -rf ~/.mozilla
+            read -p " Traces removed."
+            menu
         ;;
         2)
-
+            choiceRemoveMenu
         ;;
         *)
             choiceRemoveMenu
         ;;
         esac
 }
-menu
-    case $menuChoice in
-    0)
-        exit
-        read -p "You decided to leave the console."
-    ;;
-    1)
-        choiceRemoveMenu
-        
-        menu
-    ;;
-    2)
-        echo "Going to install a VM"
-    ;;
-    *)
-        menu
-    ;;
-    esac
+
 menu
