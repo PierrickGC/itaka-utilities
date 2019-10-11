181d02f7f9be488a1197036a8ec846106b4a414e
Kevin AMARAL <k.amaral@it-students.fr>
2019-10-11 15:30:45 +0200

    Main Menu
    largeur modifié

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-
-echo "------------------------------------------------------------------------------------"
-echo "                                                                                    "
-echo "                            Bienvenue sur itaka utilities                           "
-echo "                                                                                    "
-echo "------------------------------------------------------------------------------------"
-echo "                                                                                    "
-echo "                                                                                    "
-echo " Que voulez-vous faire ?                                                            "
-echo "                                                                                    "
-echo " 1- Test                                                                            "
+clear
+echo "--------------------------------------------------------------------------------"
+echo "                                                                                "
+echo "                         Bienvenue sur itaka utilities                          "
+echo "                                                                                "
+echo "--------------------------------------------------------------------------------"
+echo "                                                                                "
+echo "                                                                                "
+echo " Que voulez-vous faire ?                                                        "
+echo "                                                                                "
+echo " 1- Test                                                                        "
