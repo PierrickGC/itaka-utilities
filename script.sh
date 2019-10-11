f3c27a4d9a1da745bee03b16dda94a44078f41b1
Kevin AMARAL <k.amaral@it-students.fr>
2019-10-11 15:28:29 +0200

    Main Menu
    main menu form added

--- script.sh
+++ script.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 
+echo "------------------------------------------------------------------------------------"
+echo "                                                                                    "
+echo "                            Bienvenue sur itaka utilities                           "
+echo "                                                                                    "
+echo "------------------------------------------------------------------------------------"
+echo "                                                                                    "
+echo "                                                                                    "
+echo " Que voulez-vous faire ?                                                            "
+echo "                                                                                    "
+echo " 1- Test                                                                            "
