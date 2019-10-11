885acad424e0df97d29862e2161aa07b9df0c39a
Pierrick GRANDCLEMENT-CHAFFY <pierrick.grandclement@gmail.com>
2019-10-11 16:49:16 +0200

    Loop error resolved

--- script.sh
+++ script.sh
@@ -30,15 +30,7 @@ choiceRemoveMenu (){
     echo "[1] Remove all your traces"
     echo "[2] Select what you want to remove"
     read -p "What is your choice ? " removeTracesChoice
-}
-menu
-    case $menuChoice in
-    0)
-        break
-    ;;
-    1)
-        choiceRemoveMenu
-        case $removeTracesChoice in
+    case $removeTracesChoice in
         1)
             echo "All your traces are going to be revomed ..."
             rm -rf ~/.mozilla
@@ -51,6 +43,17 @@ menu
             choiceRemoveMenu
         ;;
         esac
+}
+menu
+    case $menuChoice in
+    0)
+        exit
+        read -p "You decided to leave the console."
+    ;;
+    1)
+        choiceRemoveMenu
+        
+        menu
     ;;
     2)
         echo "Going to install a VM"
@@ -59,4 +62,4 @@ menu
         menu
     ;;
     esac
-echo "You decided to leave the console."
+menu
